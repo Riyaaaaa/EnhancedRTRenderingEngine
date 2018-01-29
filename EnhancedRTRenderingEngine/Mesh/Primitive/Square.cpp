631e4ffc37e0d8d16f456e3e93e0dca89a07ba52
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-01-29 19:57:47 +0900

    Remove test code and fix square UV

--- EnhancedRTRenderingEngine/Mesh/Primitive/Square.cpp
+++ EnhancedRTRenderingEngine/Mesh/Primitive/Square.cpp
@@ -4,7 +4,7 @@
 Square::Square()
 {
 	hVectorData = std::vector<Vertex3D>{
-		Vertex3D{ { -0.75f, -0.75f, +0.5f },{ 1.0f, 1.0f, 1.0f, 1.0f },{ 0.0f, 0.1f } },
+		Vertex3D{ { -0.75f, -0.75f, +0.5f },{ 1.0f, 1.0f, 1.0f, 1.0f },{ 0.0f, 1.0f } },
 		Vertex3D{ { -0.75f, +0.75f, +0.5f },{ 1.0f, 1.0f, 1.0f, 1.0f },{ 0.0f, 0.0f } },
 		Vertex3D{ { +0.75f, -0.75f, +0.5f },{ 1.0f, 1.0f, 1.0f, 1.0f },{ 1.0f, 1.0f } },
 		Vertex3D{ { +0.75f, +0.75f, +0.5f },{ 1.0f, 1.0f, 1.0f, 1.0f },{ 1.0f, 0.0f } }
--- EnhancedRTRenderingEngine/Resource/ResourceLoader.cpp
+++ EnhancedRTRenderingEngine/Resource/ResourceLoader.cpp
@@ -46,82 +46,6 @@ ResourceHandle<> ResourceLoader::LoadShader(std::string filename) {
 	return manager->CreateCachedResourceHandle<ResourceEntity>(path, buf, size);
 }
 
-void* test(const char *filename) {
-	FILE *fp;
-	fopen_s(&fp, filename, "rb");
-	if (fp == NULL) {
-		perror(filename);
-		return NULL;
-	}
-
-	int i, x, y;
-	int width, height;
-	int num;
-	png_colorp palette;
-	png_structp png = NULL;
-	png_infop info = NULL;
-	png_bytep row;
-	png_bytepp rows;
-	png_byte sig_bytes[8];
-	if (fread(sig_bytes, sizeof(sig_bytes), 1, fp) != 1) {
-		return NULL;
-	}
-	if (png_sig_cmp(sig_bytes, 0, sizeof(sig_bytes))) {
-		return NULL;
-	}
-	png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (png == NULL) {
-		return NULL;
-	}
-	info = png_create_info_struct(png);
-	if (info == NULL) {
-		return NULL;
-	}
-	if (setjmp(png_jmpbuf(png))) {
-		return NULL;
-	}
-	png_init_io(png, fp);
-	png_set_sig_bytes(png, sizeof(sig_bytes));
-	png_read_png(png, info, PNG_TRANSFORM_PACKING | PNG_TRANSFORM_STRIP_16, NULL);
-	width = png_get_image_width(png, info);
-	height = png_get_image_height(png, info);
-	rows = png_get_rows(png, info);
-
-	png_bytep buf = new png_byte[width * height * 4 * info->channels];
-
-	switch (png_get_color_type(png, info)) {
-	case PNG_COLOR_TYPE_PALETTE:
-	case PNG_COLOR_TYPE_GRAY:
-	case PNG_COLOR_TYPE_GRAY_ALPHA:
-		// not support
-		break;
-	case PNG_COLOR_TYPE_RGB: {
-		png_bytepp rows = png_get_rows(png, info);
-		for (int y = 0; y < info->height; y++) {
-			auto* row = rows[y];
-			for (int x = 0; x < info->width; x++) {
-				memcpy(buf + y * x + x, row + x, sizeof(png_byte) * 3);
-				*(buf + y * x + x) = 0xff;
-			}
-		}
-		break;
-	}
-	case PNG_COLOR_TYPE_RGB_ALPHA: {
-		png_uint_32 i = 0;
-		int offset_point = 0;
-		while (i < info->height)
-		{
-			png_read_row(png, buf + offset_point, NULL);
-			offset_point += width * 4;
-			i++;
-		}
-		break;
-	}
-	}
-error:
-	png_destroy_read_struct(&png, &info, NULL);
-}
-
 int ResourceLoader::LoadTexture(std::string filename, ResourceHandle<Texture2D>* outTex) {
 	int w, h, d;
 	png_structp Png;
@@ -163,7 +87,6 @@ int ResourceLoader::LoadTexture(std::string filename, ResourceHandle<Texture2D>*
 
 	png_init_io(Png, fp);
 	png_set_sig_bytes(Png, sizeof(sig_bytes));
-	//png_read_info(Png, PngInfo);
 
 	png_read_png(Png, PngInfo, PNG_TRANSFORM_IDENTITY, NULL);
 
@@ -219,8 +142,6 @@ int ResourceLoader::LoadTexture(std::string filename, ResourceHandle<Texture2D>*
 		break;
 	}
 	}
-
-	//png_read_end(Png, NULL);
 
 	*outTex = manager->CreateCachedResourceHandle<Texture2D>(path, PngInfo, (void*)buf, sizeof(buf));
 
