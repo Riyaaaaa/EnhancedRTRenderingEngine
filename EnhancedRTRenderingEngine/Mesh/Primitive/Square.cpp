0218c5f62d373d786042130dce099ee20a4a1e55
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-02-07 17:27:53 +0900

    Fix SquarePMD normal vevtors

--- EnhancedRTRenderingEngine/Mesh/Primitive/Square.cpp
+++ EnhancedRTRenderingEngine/Mesh/Primitive/Square.cpp
@@ -11,7 +11,7 @@ Square::Square()
 	};
 
 	_indexList = std::vector<uint16_t>{
-		0, 1, 2, 3, 2, 1
+		0, 1, 2, 1, 3, 2
 	};
 
 	_vertexCount = _indexList.size();
@@ -19,14 +19,14 @@ Square::Square()
 
 SquarePMD::SquarePMD() {
 	_vertexList = std::vector<PMDVertex>{
-		PMDVertex{ { -0.75f, -0.75f, +0.5f },{ 0.0f, 0.0f },{ 0.0f, 1.0f, 0.0f } },
-		PMDVertex{ { -0.75f, +0.75f, +0.5f },{ 0.0f, 1.0f },{ 0.0f, 1.0f, 0.0f } },
-		PMDVertex{ { +0.75f, -0.75f, +0.5f },{ 1.0f, 0.0f },{ 0.0f, 1.0f, 0.0f } },
-		PMDVertex{ { +0.75f, +0.75f, +0.5f },{ 1.0f, 1.0f },{ 0.0f, 1.0f, 0.0f } }
+		PMDVertex{ { -0.75f, -0.75f, +0.5f },{ 0.0f, 0.0f },{ 0.0f, 0.0f, -1.0f } },
+		PMDVertex{ { -0.75f, +0.75f, +0.5f },{ 0.0f, 1.0f },{ 0.0f, 0.0f, -1.0f } },
+		PMDVertex{ { +0.75f, -0.75f, +0.5f },{ 1.0f, 0.0f },{ 0.0f, 0.0f, -1.0f } },
+		PMDVertex{ { +0.75f, +0.75f, +0.5f },{ 1.0f, 1.0f },{ 0.0f, 0.0f, -1.0f } }
 	};
 
 	_indexList = std::vector<uint16_t>{
-		0, 1, 2, 3, 2, 1
+		0, 1, 2, 1, 3, 2
 	};
 
 	_vertexCount = _indexList.size();
