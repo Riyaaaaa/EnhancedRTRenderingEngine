998ab373a3c766c3f48017342518ee47206c834a
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-02-15 19:02:39 +0900

    Add feature that parse mesh body from .x file (experimental)

--- EnhancedRTRenderingEngine/Resource/X/DXModel.h
+++ EnhancedRTRenderingEngine/Resource/X/DXModel.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+#include "Structure/Structure.h"
+
 class DXModel
 {
 public:
@@ -21,7 +23,14 @@ public:
     };
     XHeader header;
 
-    typedef std::pair<std::string, std::string> XProperty;
+    struct XProperty {
+        std::string type;
+        std::string name;
+        int size;
+    };
+
+    //typedef std::tuple<std::string, std::string, int> XProperty;
+
     struct XTemplate {
         std::string TemplateName;
         std::string UID;
@@ -29,5 +38,65 @@ public:
     };
 
     std::vector<XTemplate> templates;
+
+    struct MeshFaceWraps {
+        // UNUSED
+    };
+
+    struct MeshTextureCoords {
+        std::string UID;
+        DWORD nTextureCoords;
+        std::vector<Vector2D> textureCoords;
+    };
+
+    struct MeshFace {
+        DWORD nFaceVertexIndices;
+        std::vector<DWORD> faceVertexIndices;
+    };
+
+    struct MeshNormals {
+        DWORD nNormals;
+        std::vector<Vector3D> normals;
+        DWORD nFaceNormals;
+        std::vector<MeshFace> meshFaces;
+    };
+
+    struct IndexedColor {
+        DWORD index;
+        Vector4D indexColor;
+    };
+
+    struct MeshVertexColors {
+        DWORD nVertexColors;
+        std::vector<IndexedColor> vertexColors;
+    };
+
+    struct Material {
+        Vector4D faceColor;
+        float power;
+        Vector3D specularColor;
+        Vector3D emissiveColor;
+        std::string textureFileName;
+    };
+
+    struct MeshMaterialList {
+        DWORD nMaterials;
+        DWORD nFaceIndexes;
+        std::vector<DWORD> faceIndexes;
+        std::vector<DXModel::Material> materials;
+    } meshMaterialList;
+
+    struct Mesh {
+        DWORD nVertices;
+        std::vector<Vector3D> vertices;
+        DWORD nFaces;
+        std::vector<Vector3D> faces;
+
+        // optional data elements
+        MeshTextureCoords meshTextureCoords;
+        MeshNormals meshNormals;
+        MeshVertexColors meshVetexColors;
+        MeshMaterialList meshMaterialList;
+    } mesh;
 };
 
--- EnhancedRTRenderingEngine/Resource/X/StringParser.cpp
+++ EnhancedRTRenderingEngine/Resource/X/StringParser.cpp
@@ -1,14 +1,20 @@
 #include "stdafx.h"
+
+#define BOOST_RESULT_OF_USE_TR1
+
 #include "StringParser.h"
+#include "Common.h"
 
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
-
+#include <boost/fusion/include/boost_tuple.hpp>
 #include <boost/fusion/include/std_pair.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
 
 #include <boost/format.hpp>
 
@@ -18,6 +24,13 @@ namespace ascii = boost::spirit::ascii;
 #define TRANSFORM_STRUCT_MEMBER(ign, name, member) (decltype(name::member), member)
 #define FUSION_ADAPT_STRUCT_AUTO(name, members) \
   BOOST_FUSION_ADAPT_STRUCT(name, BOOST_PP_SEQ_FOR_EACH(TRANSFORM_STRUCT_MEMBER, name, members))
+
+FUSION_ADAPT_STRUCT_AUTO(
+    DXModel::XProperty,
+    (type)
+    (name)
+    (size)
+)
 
 FUSION_ADAPT_STRUCT_AUTO(
     DXModel::XTemplate,
@@ -32,6 +45,21 @@ FUSION_ADAPT_STRUCT_AUTO(
     (version)
     (format)
     (floatSize)
+)
+
+FUSION_ADAPT_STRUCT_AUTO(
+    Vector3D,
+    (x)
+    (y)
+    (z)
+)
+
+FUSION_ADAPT_STRUCT_AUTO(
+    Vector4D,
+    (x)
+    (y)
+    (z)
+    (w)
 )
 
 struct FileFormatSymbol_ : public qi::symbols<char, DXModel::FileFormat> {
@@ -61,27 +89,126 @@ struct XHeaderGrammar : public qi::grammar<Iterator, DXModel::XHeader()> {
 template <typename Iterator>
 struct XTemplateGrammar : public qi::grammar<Iterator, DXModel::XTemplate()> {
     XTemplateGrammar() : XTemplateGrammar::base_type(expr) {
+        // TODO: Refactor by qi::lexeme
         expr = qi::omit[*qi::space] >> qi::lit("template") >> *qi::lit(' ') >> +(qi::char_ - (qi::lit('{') | qi::space)) >> qi::omit[*qi::space] >> qi::lit('{') >> qi::omit[*qi::space]
+            // PARSE UID
             >> qi::lit('<') >> *qi::lit(' ') >> +(qi::char_ - (qi::lit('>') | qi::space)) >> *qi::lit(' ') >> qi::lit('>') >> qi::omit[*qi::space]
-            >> +(+(qi::char_ - qi::lit(' ')) >> +qi::lit(' ') >> +(qi::char_ - (qi::space | qi::lit(';'))) >> *qi::lit(' ') >> qi::lit(';') >> qi::omit[*qi::space])
-            >> qi::lit('}');
+            // PARSE PROPERTIES
+            >> +prop >> qi::omit[*(qi::char_ - qi::lit('}'))] >> qi::lit('}');
+
+        // PROPERTY RULE
+        prop = -qi::lit("array") >> *qi::lit(' ') >> +(qi::char_ - qi::lit(' ')) >> +qi::lit(' ') >> +(
+                    qi::char_ - (qi::space | qi::lit(';') | qi::lit('['))
+                ) >> *qi::lit(' ') >> ((qi::lit('[') >> qi::int_ >> qi::lit(']')) | qi::attr(1)) >> *qi::lit(' ') >> qi::lit(';') >> qi::omit[*qi::space];
     }
 
     qi::rule<Iterator, DXModel::XTemplate()> expr;
+    qi::rule<Iterator, DXModel::XProperty()> prop;
 };
 
 template <typename Iterator>
 struct NextDataIdentifierGrammar : public qi::grammar<Iterator, std::string()> {
     NextDataIdentifierGrammar(const std::vector<DXModel::XTemplate>& xtemplates) : NextDataIdentifierGrammar::base_type(expr) {
         for (auto && xtemplate : xtemplates) {
-            templateParser |= ascii::string(xtemplate.TemplateName);
+            templateParser = templateParser.copy() | ascii::string(xtemplate.TemplateName);
         }
 
-        expr = *(qi::char_ - templateParser) >> templateParser;
+        expr = qi::omit[*(qi::char_ - templateParser)] >> templateParser;
     }
     qi::rule<Iterator, std::string()> expr;
     qi::rule<Iterator, std::string()> templateParser;
 };
+
+template <typename Iterator>
+struct TextureFileNameGrammer : public qi::grammar<Iterator, std::string(), qi::space_type> {
+    TextureFileNameGrammer() : TextureFileNameGrammer::base_type(expr)
+    {
+        expr = (qi::lit('{') 
+            >> qi::lit('"') >> *(qi::char_ - qi::lit('"')) >> qi::lit('"') >> qi::lit(';') 
+            >> qi::lit('}')) | qi::attr("");
+    }
+
+    qi::rule<Iterator, std::string(), qi::space_type> expr;
+};
+
+//template <typename Iterator>
+//struct MeshBodyGrammar : public qi::grammar<Iterator, DXModel::Mesh> {
+//
+//};
+
+bool ParseDataCommand(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model);
+
+void ExecAllCommandCurrentBody(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model) {
+    while (true) {
+        ParseDataCommand(itr, end, model);
+        bool isEnd = qi::phrase_parse(itr, end, qi::lit('}'), qi::space);
+
+        if (isEnd) {
+            break;
+        }
+    }
+}
+
+void parseMeshMaterialList(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model) {
+    qi::phrase_parse(itr, end, qi::lit('{'), qi::space);
+    qi::phrase_parse(itr, end, qi::int_ >> qi::lit(';'), qi::space, model->meshMaterialList.nMaterials);
+    qi::phrase_parse(itr, end, qi::int_ >> qi::lit(';'), qi::space, model->meshMaterialList.nFaceIndexes);
+    qi::phrase_parse(itr, end, qi::int_ % ',' >> qi::lit(';'), qi::space , model->meshMaterialList.faceIndexes);
+    qi::phrase_parse(itr, end, qi::lit(';'), qi::space);
+
+    //parse optioal params;
+    ExecAllCommandCurrentBody(itr, end, model);
+}
+
+void parseMaterial(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model) {
+    DXModel::Material material;
+    qi::phrase_parse(itr, end, qi::lit('{'), qi::space);
+    qi::phrase_parse(itr, end, qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';'), qi::space, material.faceColor);
+    qi::phrase_parse(itr, end, qi::float_ >> qi::lit(';'), qi::space);
+    qi::phrase_parse(itr, end, qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::lit(';'), qi::space, material.specularColor);
+    qi::phrase_parse(itr, end, qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::lit(';'), qi::space, material.emissiveColor);
+    TextureFileNameGrammer<decltype(itr)> parser;
+    qi::phrase_parse(itr, end, parser, qi::space, material.textureFileName);
+    model->meshMaterialList.materials.push_back(material);
+}
+
+void parseMeshBody(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model) {
+    qi::phrase_parse(itr, end, qi::lit('{'), qi::space);
+    qi::phrase_parse(itr, end, qi::int_ >> qi::lit(';'), qi::space, model->mesh.nVertices);
+    qi::phrase_parse(itr, end, (qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';') >> qi::float_ >> qi::lit(';')) % ',' >> qi::lit(';') , qi::space, model->mesh.vertices);
+    
+    ExecAllCommandCurrentBody(itr, end, model);
+}
+
+bool ParseDataCommand(std::string::const_iterator itr, const std::string::const_iterator end, DXModel* model) {
+#define BindCommand(func) std::bind(func, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
+    static std::unordered_map<std::string, std::function<void(std::string::const_iterator, const std::string::const_iterator, DXModel*)>> commands = {
+        std::make_pair("Mesh", BindCommand(parseMeshBody)),
+        std::make_pair("MeshMaterialList", BindCommand(parseMeshMaterialList)),
+        std::make_pair("Material", BindCommand(parseMaterial))
+    };
+#undef BindCommand
+
+    NextDataIdentifierGrammar<std::string::const_iterator> parser(model->templates);
+    std::string templateIdentifier;
+    bool r = qi::parse(itr, end, parser, templateIdentifier);
+
+    if (r) {
+        auto commandItr = commands.find(templateIdentifier);
+        if (commandItr != commands.end()) {
+            (*commandItr).second(itr, end, model);
+            return true;
+        }
+        else {
+            ERTREDebug(L"Found not undefined template identifier: %hs", templateIdentifier.c_str());
+            // skip body;
+            qi::phrase_parse(itr, end, *(qi::char_ - qi::lit('}')) >> qi::lit('}'), qi::space);
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int StringParser::ParseXFile(std::ifstream& ifs, DXModel* model) {
     std::string buffer((std::istreambuf_iterator<char>(ifs)),  std::istreambuf_iterator<char>());
@@ -109,19 +236,7 @@ int StringParser::ParseXFile(std::ifstream& ifs, DXModel* model) {
     }
 
     {
-        NextDataIdentifierGrammar<std::string::iterator> parser(model->templates);
-        std::string templateIdentifier;
-        while (true) {
-            bool r = qi::parse(itr, end, parser, templateIdentifier);
-
-            if (r) {
-                std::cout << templateIdentifier << std::endl;
-                // TODO: parse data
-            }
-            if (!r || itr == end) {
-                break;
-            }
-        }
+        ParseDataCommand(itr, end, model);
     }
 
     return 0;
