43e834353a52cc2ad92d8f1b207dda9cfa1d1e31
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-04-09 15:50:58 +0900

    Add shaders for HUD rendering

--- EnhancedRTRenderingEngine/Shader/HUD/HUDPixelShader.hlsl
+++ EnhancedRTRenderingEngine/Shader/HUD/HUDPixelShader.hlsl
@@ -1,4 +1,21 @@
-float4 main() : SV_TARGET
+sampler sampler0 : register(s0);
+Texture2D<float4> texture0 : register(t0);
+
+struct VS_INPUT
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float2 pos : POSITION;
+    float4 col : COLOR0;
+    float2 uv  : TEXCOORD0;
+};
+
+struct PS_INPUT
+{
+    float4 pos : SV_POSITION;
+    float4 col : COLOR0;
+    float2 uv  : TEXCOORD0;
+};
+
+float4 main(PS_INPUT input) : SV_Target
+{
+    return input.col * texture0.Sample(sampler0, input.uv);
 }
