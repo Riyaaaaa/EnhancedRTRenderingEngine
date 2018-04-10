20101eee5fd4eb4108688cf62d772cd47c69ab92
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-04-10 11:06:47 +0900

    Add dimension extensibility to vector class

--- EnhancedRTRenderingEngine/Structure/Vector.h
+++ EnhancedRTRenderingEngine/Structure/Vector.h
@@ -2,93 +2,115 @@
 
 #include <cmath>
 
-struct Vector2D {
-    float x, y;
+template <class T, std::size_t dim>
+struct _Vector {
+    float operator[](unsigned int idx) const {
+        T* ptr = reinterpret_cast<T*>(this);
+        return *(ptr + idx);
+    }
+    float& operator[](unsigned int idx) {
+        T* ptr = reinterpret_cast<T*>(this);
+        return *(ptr + idx);
+    }
+private:
+    T _elems[dim];
+};
 
-    Vector2D()
-        : x(0.0f), y(0.0f)
+template<std::size_t dim>
+using Vector = _Vector<float, dim>;
+
+template<class T>
+struct _Vector<T, 2> {
+    T x, y;
+
+    _Vector()
+        : x(0), y(0)
     {}
 
-    Vector2D(float xx, float yy)
+    _Vector(T xx, T yy)
         : x(xx), y(yy)
     {}
 
-    float distance(const Vector2D& v) const {
+    float distance(const _Vector& v) const {
         return sqrtf(powf(v.x - this->x, 2) + powf(v.y - this->y, 2));
     }
 
-    Vector2D operator+(const Vector2D& v) const {
-        return Vector2D(x + v.x, y + v.y);
+    _Vector operator+(const _Vector& v) const {
+        return _Vector(x + v.x, y + v.y);
     }
 
-    Vector2D operator-(const Vector2D &v) const {
-        return Vector2D(this->x - v.x, this->y - v.y);
+    _Vector operator-(const _Vector &v) const {
+        return _Vector(this->x - v.x, this->y - v.y);
     }
 
-    Vector2D operator-() const {
-        return Vector2D(-this->x, -this->y);
+    _Vector operator-() const {
+        return _Vector(-this->x, -this->y);
     }
 
-    Vector2D operator*(float s) const {
-        return Vector2D(this->x * s, this->y * s);
+    _Vector operator*(float s) const {
+        return _Vector(this->x * s, this->y * s);
     }
 
-    Vector2D operator/(float s) const {
-        return Vector2D(this->x / s, this->y / s);
+    _Vector operator/(float s) const {
+        return _Vector(this->x / s, this->y / s);
     }
 
-    bool operator<(const Vector2D& v) const {
+    bool operator<(const _Vector& v) const {
         return (x == v.x) ? y < v.y : x < v.x;
     }
 
-    bool operator>(const Vector2D& v) const {
+    bool operator>(const _Vector& v) const {
         return (x == v.x) ? y > v.y : x > v.x;
     }
 
-    bool operator==(const Vector2D& v) const {
+    bool operator==(const _Vector& v) const {
         return x == v.x && y == v.y;
     }
 
-    bool operator!=(const Vector2D& v) const {
+    bool operator!=(const _Vector& v) const {
         return x != v.x || y != v.y;
     }
 
-    Vector2D& operator-=(const Vector2D& v) {
+    _Vector& operator-=(const _Vector& v) {
         this->x -= v.x;
         this->y -= v.y;
         return *this;
     }
 
-    Vector2D& operator+=(const Vector2D& v) {
+    _Vector& operator+=(const _Vector& v) {
         this->x = this->x + v.x;
         this->y = this->y + v.y;
 
         return *this;
     }
 
-    Vector2D& operator*=(float s) {
+    _Vector& operator*=(float s) {
         this->x *= s;
         this->y *= s;
         return *this;
     }
 
-    Vector2D& operator/=(float s) {
+    _Vector& operator/=(float s) {
         this->x /= s;
         this->y /= s;
         return *this;
     }
 };
 
+using Vector2D = _Vector<float, 2>;
 template<class T>
-struct _Vector3D {
+using _Vector2D = _Vector<T, 2>;
+
+template<class T>
+struct _Vector<T, 3> {
     T x, y, z;
 
-    _Vector3D() {}
-    constexpr _Vector3D(T x, T y, T z) : x(x), y(y), z(z) {}
+    _Vector() {}
+    constexpr _Vector(T x, T y, T z) : x(x), y(y), z(z) {}
 
     template<class U>
-    _Vector3D operator-(const _Vector3D<U> &v) const {
-        return _Vector3D{
+    _Vector operator-(const _Vector<U, 3> &v) const {
+        return _Vector{
             static_cast<T>(this->x - v.x),
             static_cast<T>(this->y - v.y),
             static_cast<T>(this->z - v.z)
@@ -96,8 +118,8 @@ struct _Vector3D {
     }
 
     template<class U>
-    _Vector3D operator+(const _Vector3D<U> &v) const {
-        return _Vector3D{ 
+    _Vector operator+(const _Vector<U, 3> &v) const {
+        return _Vector{
             static_cast<T>(this->x + v.x), 
             static_cast<T>(this->y + v.y),
             static_cast<T>(this->z + v.z)
@@ -105,23 +127,23 @@ struct _Vector3D {
     }
 
     template<class U>
-    operator _Vector3D<U>() {
-        return _Vector3D<U>(
+    operator _Vector<U, 3>() {
+        return _Vector<U, 3>(
             static_cast<U>(x),
             static_cast<U>(y),
             static_cast<U>(z)
             );
     }
 
-    _Vector3D operator-() const {
-        return _Vector3D{ -this->x, -this->y, -this->z };
+    _Vector operator-() const {
+        return _Vector{ -this->x, -this->y, -this->z };
     }
 
-    _Vector3D operator*(float s) const {
-        return _Vector3D{ this->x * s, this->y * s, this->z * s };
+    _Vector operator*(float s) const {
+        return _Vector{ this->x * s, this->y * s, this->z * s };
     }
 
-    _Vector3D& operator+=(const _Vector3D& v) {
+    _Vector& operator+=(const _Vector& v) {
         this->x = this->x + v.x;
         this->y = this->y + v.y;
         this->z = this->z + v.z;
@@ -140,16 +162,41 @@ struct _Vector3D {
     }
 };
 
-using Vector3D = _Vector3D<float>;
+using Vector3D = _Vector<float, 3>;
+template<class T>
+using _Vector3D = _Vector<T, 3>;
 
 template <class T>
-struct _Vector4D {
-    _Vector4D() {}
-    constexpr _Vector4D(T x, T y, T z, T w = 1) : x(x), y(y), z(z), w(w) {}
-    constexpr _Vector4D(const _Vector3D<T> vec3) : x(vec3.x), y(vec3.y), z(vec3.z), w(1) {}
+struct _Vector<T, 4> {
+    _Vector() {}
+    float operator[](unsigned int idx) const {
+        T* ptr = reinterpret_cast<T*>(this);
+        return *(ptr + idx);
+    }
+    float& operator[](unsigned int idx) {
+        T* ptr = reinterpret_cast<T*>(this);
+        return *(ptr + idx);
+    }
+    constexpr _Vector(T x, T y, T z, T w = 1) : x(x), y(y), z(z), w(w) {}
+    constexpr _Vector(const _Vector<T, 3> vec3) : x(vec3.x), y(vec3.y), z(vec3.z), w(1) {}
     T x, y, z, w;
 
-    static constexpr _Vector4D Zero() { return _Vector4D(0, 0, 0); }
+    static constexpr _Vector Zero() { return _Vector(0, 0, 0, 0); }
 };
 
-using Vector4D = _Vector4D<float>;
+using Vector4D = _Vector<float, 4>;
+template<class T>
+using _Vector4D = _Vector<T, 4>;
+
+struct Matrix {
+    const Vector4D& operator[](unsigned int idx) const {
+        return _elems[idx];
+    }
+
+    Vector4D& operator[](unsigned int idx) {
+        return _elems[idx];
+    }
+
+private:
+    Vector4D _elems[4];
+};
