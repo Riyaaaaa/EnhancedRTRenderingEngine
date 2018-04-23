7bfb8258dfb879f9eefebec606629f6dbf413fdb
Atsumu Ono <s113111@edu.asahikawa-nct.ac.jp>
2018-04-23 10:43:49 +0900

    Fix rasterizing triangle

--- EnhancedRTRenderingEngine/Mesh/MeshExpander.cpp
+++ EnhancedRTRenderingEngine/Mesh/MeshExpander.cpp
@@ -16,7 +16,7 @@ MeshExpander::MeshExpander(unsigned int size, unsigned int margin) :
 
 }
 
-static void RasterizeTriangle(ExpandMap& map, const std::array<_Vector2D<long>, 3>& rasterPositions, Triangle& tri, int idx) {
+static void RasterizeTriangle01(ExpandMap& map, const std::array<_Vector2D<long>, 3>& rasterPositions, Triangle& tri, int idx) {
     float a = (rasterPositions[2].y - rasterPositions[1].y) / (float)(rasterPositions[2].x - rasterPositions[1].x);
     float b = rasterPositions[1].y - a * rasterPositions[1].x;
 
@@ -31,9 +31,39 @@ static void RasterizeTriangle(ExpandMap& map, const std::array<_Vector2D<long>, 
     float dot11 = Dot(ab, ab);
     float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
 
-    for (long y = rasterPositions[0].y; y < rasterPositions[2].y; y++) {
-        for (long x = rasterPositions[0].x; x < (y - b) / a; x++) {
+    for (long y = rasterPositions[0].y; y <= rasterPositions[2].y; y++) {
+        for (long x = rasterPositions[0].x; x <= (y - b) / a; x++) {
             
+            _Vector2D<long> ap = _Vector2D<long>(x, y) - rasterPositions[0];
+            float dot02 = Dot(ac, ap);
+            float dot12 = Dot(ab, ap);
+
+            float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+            float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+            map(x, y).worldPosition = v2v0 * u + v1v0 * v;
+            map(x, y).belongsTriangleIdx = idx;
+        }
+    }
+}
+
+static void RasterizeTriangle02(ExpandMap& map, const std::array<_Vector2D<long>, 3>& rasterPositions, Triangle& tri, int idx) {
+    float a = (rasterPositions[1].y - rasterPositions[0].y) / (float)(rasterPositions[1].x - rasterPositions[0].x);
+    float b = rasterPositions[0].y - a * rasterPositions[0].x;
+
+    _Vector2D<long> ac = rasterPositions[2] - rasterPositions[0];
+    _Vector2D<long> ab = rasterPositions[1] - rasterPositions[0];
+
+    Vector4D v2v0 = tri.v2 - tri.v0;
+    Vector4D v1v0 = tri.v1 - tri.v0;
+
+    float dot00 = Dot(ac, ac);
+    float dot01 = Dot(ac, ab);
+    float dot11 = Dot(ab, ab);
+    float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+
+    for (long y = rasterPositions[0].y; y >= rasterPositions[1].y; y--) {
+        for (long x = (y - b) / a; x <= rasterPositions[1].x; x++) {
             _Vector2D<long> ap = _Vector2D<long>(x, y) - rasterPositions[0];
             float dot02 = Dot(ac, ap);
             float dot12 = Dot(ab, ap);
@@ -68,25 +98,25 @@ ExpandMap MeshExpander::Build(MeshBase* mesh) {
             offsetX += _margin;
             mappedVertPositions = std::array<_Vector2D<long>, 3> {
                 _Vector2D<long>(offsetX, offsetY),
-                    _Vector2D<long>((offsetX + bake_triangle_size), offsetY),
-                    _Vector2D<long>(offsetX, (offsetY + bake_triangle_size))
+                    _Vector2D<long>((offsetX + bake_triangle_size - 1), offsetY),
+                    _Vector2D<long>(offsetX, (offsetY + bake_triangle_size - 1))
             };
+            RasterizeTriangle01(map, mappedVertPositions, tri, i);
         }
         else {
             mappedVertPositions = std::array<_Vector2D<long>, 3> {
-                _Vector2D<long>(offsetX, (offsetY + bake_triangle_size)),
-                    _Vector2D<long>((offsetX + bake_triangle_size), (offsetY + +bake_triangle_size)),
-                    _Vector2D<long>((offsetX + bake_triangle_size), offsetY)
+                _Vector2D<long>(offsetX, (offsetY + bake_triangle_size - 1)),
+                    _Vector2D<long>((offsetX + bake_triangle_size - 1), offsetY),
+                    _Vector2D<long>((offsetX + bake_triangle_size - 1), (offsetY + bake_triangle_size - 1))
             };
-
+            RasterizeTriangle02(map, mappedVertPositions, tri, i);
             offsetX += bake_triangle_size;
-            if (offsetX >= _expandSize) {
+            if (offsetX >= _expandSize - bake_triangle_size - _margin) {
                 offsetX = 0;
                 offsetY += bake_triangle_size + _margin;
             }
         }
-
-        RasterizeTriangle(map, mappedVertPositions, tri, i);
+ 
         map.AddTriangle(mappedVertPositions[0], mappedVertPositions[1], mappedVertPositions[2]);
     }
 
